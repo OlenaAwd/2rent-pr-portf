5e8f1852d460dbc1c41f8e85d000a134e653fef6
OlenaAwd <elena.avdiunina@gmail.com>
2021-07-09 16:06:13 +0300

    add sections

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(function(){
-$('.slider').slick({});
+$('.slider').slick({
+    nextArrow: '<button class="slick-arrow slick-next"><img src="./images/arrow-next.svg" alt="arrow-next"></button>',
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="./images/arrow-prev.svg" alt="arrow-prev"></button>',
+    fade:true
 });
+});
+
+
+
+
+  
