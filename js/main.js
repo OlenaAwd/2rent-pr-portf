e9f33e0669b1409f596f9a232e6148d11c5c03d4
Olena Avdiunina <elena.avdiunina@gmail.com>
2021-07-12 17:47:05 +0300

    finish html + css

--- js/main.js
+++ js/main.js
@@ -2,7 +2,15 @@ $(function(){
 $('.slider').slick({
     nextArrow: '<button class="slick-arrow slick-next"><img src="./images/arrow-next.svg" alt="arrow-next"></button>',
     prevArrow: '<button class="slick-arrow slick-prev"><img src="./images/arrow-prev.svg" alt="arrow-prev"></button>',
-    fade:true
+    fade:true,
+    responsive: [
+        {
+            breakpoint: 441,
+            settings:{
+                arrows: false
+            }
+        }
+    ]
 });
 });
 
